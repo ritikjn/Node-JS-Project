454e4f281e4be84d079213b8b838a513a8068070
ritikjn <ritikjain464@gmail.com>
2022-05-27 11:47:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+fetch("http://universities.hipolabs.com/search?country=United+States").then((data)=>{
+    return data.json(); //converted from string array data to object
+}).then((objectData)=>{
+    console.log(objectData);
+    let tableData = "";
+    objectData.map((values)=>{
+        tableData+=`<tr>
+        <td>${values.alpha_two_code}</td>
+        <td>${values.domains}</td>
+        <td>${values.country}</td>
+        <td>${values["state-province"]}</td>
+        <td>${values.web_pages}</td>
+        <td>${values.name}</td>
+      </tr>`;
+    });
+
+    document.getElementById("table_content").innerHTML=tableData;  
+})
+
+
